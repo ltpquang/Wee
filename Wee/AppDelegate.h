8fbce6575446f9c05433b201ef7ce0a44f50be6b
Le Thai Phuc Quang <phuc.quang102@gmail.com>
2015-05-23 10:25:50 +0700

    Initial Commit

---
+++ Wee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wee
+//
+//  Created by Le Thai Phuc Quang on 5/23/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Wee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wee
+//
+//  Created by Le Thai Phuc Quang on 5/23/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
